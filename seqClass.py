e817bf3409d654fd14966b5a653c08547b723910
Sergi Soldevila <sergi.soldevila@uvic.cat>
2023-02-13 17:55:12 +0100

    task12

--- seqClass.py
+++ seqClass.py
@@ -3,8 +3,9 @@
 import sys, re
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
-parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+parser = ArgumentParser(description="Classify a sequence as DNA or RNA")
+parser.add_argument("-s", "--seq", type=str, required=True, help="Input sequence")
+parser.add_argument("-m", "--motif", type=str, required=False, help="Motif")
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -12,14 +13,24 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-args.seq = args.seq.upper()                 # Note we just added this line
-if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
-        print ('The sequence is DNA')
-    elif re.search('U', args.seq):
-        print ('The sequence is RNA')
+args.seq = args.seq.upper()  # Note we just added this line
+if re.search("^[ACGTU]+$", args.seq):
+    if re.search("T", args.seq):
+        print("The sequence is DNA")
+    elif re.search("U", args.seq):
+        print("The sequence is RNA")
     else:
-        print ('The sequence can be DNA or RNA')
+        print("The sequence can be DNA or RNA")
 else:
-    print ('The sequence is not DNA nor RNA')#!/usr/bin/env python
+    print("The sequence is not DNA nor RNA")  #!/usr/bin/env python
 
+if args.motif:
+    args.motif = args.motif.upper()
+    print(
+        f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ',
+        end="",
+    )
+    if re.search(args.motif, args.seq):
+        print("FOUND")
+    else:
+        print("NOT FOUND")
